289b56cdae782d9db561a3b79d1075d4059340ac
Max Glenister <moglenstar@gmail.com>
2017-07-17 18:52:31 +0100

    Added an 'export' button

--- js/index.js
+++ js/index.js
@@ -4,24 +4,24 @@
 
   var BPM = 120;
   var TICKS = 16;
-  var soundPrefix = 'https://blog.omgmog.net/beatmaker/';
+  var soundPrefix = 'https://blog.omgmog.net/beatmaker/sounds/';
   var sounds = [
-    'sounds/bass_drum.wav',
-    'sounds/snare_drum.wav',
-    'sounds/low_tom.wav',
-    'sounds/mid_tom.wav',
-    'sounds/hi_tom.wav',
-    'sounds/rim_shot.wav',
-    'sounds/hand_clap.wav',
-    'sounds/cowbell.wav',
-    'sounds/cymbal.wav',
-    'sounds/o_hi_hat.wav',
-    'sounds/cl_hi_hat.wav',
-    'sounds/low_conga.wav',
-    'sounds/mid_conga.wav',
-    'sounds/hi_conga.wav',
-    'sounds/claves.wav',
-    'sounds/maracas.wav'
+    'bass_drum.wav',
+    'snare_drum.wav',
+    'low_tom.wav',
+    'mid_tom.wav',
+    'hi_tom.wav',
+    'rim_shot.wav',
+    'hand_clap.wav',
+    'cowbell.wav',
+    'cymbal.wav',
+    'o_hi_hat.wav',
+    'cl_hi_hat.wav',
+    'low_conga.wav',
+    'mid_conga.wav',
+    'hi_conga.wav',
+    'claves.wav',
+    'maracas.wav'
   ];
   var buffers = {};
   if (AudioContext) {
@@ -75,6 +75,10 @@
       if (c === 0) {
         _$button.classList.add('first');
       }
+      // add a class based on the instrument
+      var soundname = sounds[r].split('.')[0];
+      _$button.classList.add(soundname);
+      _$button.dataset.instrument = soundname;
 
       _$button.addEventListener('click', function() {
         this.classList.toggle('on');
@@ -112,6 +116,54 @@
     }
   };
   document.querySelector('#random').addEventListener('click', setRandomBeat);
+
+  var exportBeat = function () {
+    // create an object so we can jsonify it later
+    var exportData = {}
+    // for each row (sound)
+    sounds.forEach(function (sound) {
+      // get the soundname, without .wav
+      var soundname = sound.split('.')[0];
+
+      // create arrays
+      var cellsgrouped = [];
+
+      // this will give us [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+      var cellsbuffer = Array.apply(null, Array(TICKS)).map(Number.prototype.valueOf,0);
+
+      // set the size of a group
+      var groupsize = 4;
+
+      // select all of the cells in the beat maker by the current soundname
+      var cells = document.querySelectorAll(`.beat.${soundname}`);
+
+      // loop over the cells
+      for (var i=0; i<cells.length;i++) {
+        // if it's on, the value is 1
+        if (cells[i].classList.contains('on')) {
+          cellsbuffer[i] = 1;
+        }
+      }
+      // group the cells in to sets
+      // this will give us [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+      while (cellsbuffer.length > 0) {
+        cellsgrouped.push(cellsbuffer.splice(0, groupsize));
+      }
+      // update the object
+      exportData[soundname] = cellsgrouped;
+
+    });
+
+    // create json that can be downloaded
+    var data_string = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportData));
+    var download_link = document.getElementById('download_link');
+    download_link.setAttribute("href", data_string);
+    download_link.setAttribute("download", "tracks.json");
+
+    // trigger download
+    download_link.click();
+  };
+  document.querySelector('#export').addEventListener('click', exportBeat);
 
   var currentTick = 0;
   var lastTick = TICKS - 1;
